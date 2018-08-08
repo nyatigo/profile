76c4b3aea5b02f3768f5c8daae82b09fa70d17fc
nyatigo <sundaytigo@gmail.com>
2018-08-07 18:17:00 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//first names
+var array1 = ["sabrina", "mario", "kristopher", "allan", "nicole", "krizia", "kenneth", "skarli"];
+//last names
+var array2 = ["hall", "ayar", "rougeau", "nyatigo", "duff", "limpin", "gary", "pena"];
+//student ID
+var array3 = ["00124", "00125", "00126", "00127", "00128", "00129", "00130", "00131"];
+//images
+var array4 = ['https://i.pinimg.com/originals/23/10/37/23103793c616e7d8105ee74f53d143dd.jpg', 'https://i.pinimg.com/originals/a2/08/f9/a208f977712baa2399ceff1564d9fa57.jpg', 'https://img.buzzfeed.com/buzzfeed-static/static/2015-09/18/12/tmp/webdr15/a50607fdb38763f9c9e06958bb1bffbc-6.png?downsize=715:*&output-format=auto&output-quality=auto', 'http://images.nymag.com/images/2/daily/2009/09/20090921_smithers_250x375.jpg', 'https://pbs.twimg.com/media/CPEDbx8WIAA1LUb.jpg', 'http://cdn.playbuzz.com/cdn/20bec706-9007-4085-83cd-84a21a0aec5c/7363f9aa-2486-49ea-8b21-feb15ce2ea94.jpg', 'https://vignette.wikia.nocookie.net/simpsons/images/6/6a/Mr_Burns_evil.gif/revision/latest?cb=20100702150413', 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/C-bob.png/220px-C-bob.png'];
+
+//student objects
+var students = [];
